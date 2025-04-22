23960f4144df25994b9fb90b647bc94c2feae6ed
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-22 16:36:03 +0400

    chore: Register page

--- App.tsx
+++ App.tsx
@@ -8,11 +8,11 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 import {fab} from '@fortawesome/free-brands-svg-icons';
 import {faSquareCheck} from '@fortawesome/free-solid-svg-icons/faSquareCheck';
 import {faMugSaucer} from '@fortawesome/free-solid-svg-icons/faMugSaucer';
+import RegisterPage from './pages/RegisterPage';
 // import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 // import {faMugEmpty} from '@fortawesome/free-solid-svg-icons/faMugEmpty';
 
 library.add(fab, faSquareCheck, faMugSaucer);
-
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
@@ -29,6 +29,19 @@ function App(): React.JSX.Element {
           component={LoginPage}
           options={{
             title: 'Log In',
+            headerTitleAlign: 'center',
+            headerTintColor: '#2260ff',
+            headerTitleStyle: {
+              fontFamily: 'POPPINS',
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="RegisterPage"
+          component={RegisterPage}
+          options={{
+            title: 'New Account',
             headerTitleAlign: 'center',
             headerTintColor: '#2260ff',
             headerTitleStyle: {
--- pages/RegisterPage.js
+++ pages/RegisterPage.js
@@ -1,16 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native';
 import CustomInput from '../components/CustomInput/CustomInput';
 import Button from '../components/Buttons/Button';
+import {useNavigation} from '@react-navigation/native';
 import SocialNetworkComponent from '../components/SocialNetworks/SocialNetworksComponent';
 import PoliciesComponent from '../components/Policies/PoliciesComponent';
-import {useNavigation} from '@react-navigation/native';
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.loginPageContainer}>
+    <View style={styles.registerPageContainer}>
       <View style={styles.margin}>
+        <CustomInput
+          label="Full Name"
+          placeholder="Enter your full name"
+          keyboardType="name"
+        />
+
         <CustomInput
           label="Email"
           placeholder="email@example.com"
@@ -22,22 +28,25 @@ const LoginPage = () => {
           placeholder="Type your password..."
           isPassword={true}
         />
+
+        <CustomInput
+          label="Password confirmation"
+          placeholder="Confirm your password..."
+          isPassword={true}
+        />
       </View>
-      <Text style={[styles.marginForgot, styles.textColor]}>
-        Forgot password?
-      </Text>
       <View style={styles.buttonSection}>
-        <Button text="Log in" />
+        <Button text="Sign Up" />
       </View>
 
       <View style={styles.loginSection}>
         <Text style={styles.loginSectionColor}>
-          Do not have a Galactic ID?{' '}
+          Already have a Galactic ID?{' '}
         </Text>
         <Text
-          style={styles.loginBtn}
-          onPress={() => navigation.navigate('RegisterPage')}>
-          Register
+          onPress={() => navigation.navigate('LoginPage')}
+          style={styles.loginBtn}>
+          Log in.
         </Text>
       </View>
       <View style={styles.centeredDiv}>
@@ -53,8 +62,10 @@ const LoginPage = () => {
   );
 };
 
+export default RegisterPage;
+
 const styles = StyleSheet.create({
-  loginPageContainer: {
+  registerPageContainer: {
     flex: 1,
     backgroundColor: '#ffffff',
   },
@@ -63,27 +74,10 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
   },
-  marginForgot: {
-    marginLeft: 10,
-  },
-  textColor: {
-    color: '#4e576a',
-    fontFamily: 'POPPINS',
-  },
   buttonSection: {
-    marginTop: 30,
+    marginTop: 20,
     marginLeft: 10,
     marginRight: 10,
-  },
-  centeredDiv: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textOr: {
-    color: '#6b6b6b',
-    fontFamily: 'POPPINS',
-    marginTop: 10,
   },
   loginSection: {
     display: 'flex',
@@ -99,16 +93,23 @@ const styles = StyleSheet.create({
     color: 'rgba(74, 95, 255, 1)',
     fontFamily: 'Poppins',
   },
+  centeredDiv: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textOr: {
+    color: '#6b6b6b',
+    fontFamily: 'POPPINS',
+  },
   socialMargin: {
-    marginTop: 20,
+    marginTop: 40,
   },
   footer: {
     position: 'absolute',
-    bottom: 40,
+    bottom: 50,
     left: 0,
     right: 0,
     padding: 16,
   },
 });
-
-export default LoginPage;
