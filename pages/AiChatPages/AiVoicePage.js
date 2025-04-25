ee2bdb16e0f58fd592e6c3f3cbabac075b104236
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-25 12:55:52 +0400

    style: microphone style in ai voice page shadows

--- pages/AiChatPages/AiVoicePage.js
+++ pages/AiChatPages/AiVoicePage.js
@@ -1,7 +1,98 @@
-import {View} from 'react-native';
-
+import {StyleSheet, View} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faMicrophone} from '@fortawesome/free-solid-svg-icons';
+import LinearGradient from 'react-native-linear-gradient';
 const AiVoicePage = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={[styles.shadow, styles.leftShadow]} />
+
+        {/* Sağ Gölge */}
+        <View style={[styles.shadow, styles.rightShadow]} />
+
+        {/* Alt Gölge */}
+        <View style={[styles.shadow, styles.bottomShadow]} />
+        <LinearGradient
+          colors={['#376deb', '#2951e6']}
+          style={styles.circleMic}
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 1}}>
+          <LinearGradient
+            style={styles.circleMicSec}
+            colors={['#7395fd', '#2352e8']}
+            start={{x: 0, y: 0}}
+            end={{x: 1, y: 1}}>
+            <FontAwesomeIcon size={30} icon={faMicrophone} color="white" />
+          </LinearGradient>
+        </LinearGradient>
+      </View>
+    </View>
+  );
 };
 
 export default AiVoicePage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  circleMic: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 130,
+    height: 130,
+    backgroundColor: '#2a52e6',
+    borderRadius: 100,
+    padding: 10,
+  },
+  circleMicSec: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    padding: 5,
+  },
+  micSizeTop: {
+    fontSize: 100,
+  },
+  shadow: {
+    position: 'absolute',
+    width: 130,
+    height: 130,
+    borderRadius: 100,
+    opacity: 0.7,
+    shadowOffset: {
+      width: 20,
+      height: 20,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 80,
+    filter: 'blur(2px)',
+  },
+  leftShadow: {
+    backgroundColor: '#a976e1',
+    left: -10,
+    top: -11,
+  },
+  rightShadow: {
+    backgroundColor: '#5054c1',
+    right: -12,
+    top: 0,
+  },
+  bottomShadow: {
+    backgroundColor: '#93d9e2',
+    bottom: -12,
+    left: -6,
+    right: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+});
