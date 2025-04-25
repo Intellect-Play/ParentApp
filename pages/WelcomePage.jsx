72893612d13e4712c925e4ccb255310b3211f7bf
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-25 16:07:21 +0400

    refactor: Welcome Page

--- pages/WelcomePage.jsx
+++ pages/WelcomePage.jsx
@@ -57,8 +57,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    // justifyContent: 'center',
-    // alignItems: 'center',
     backgroundColor: '#ffffff',
     fontFamily: 'Poppins',
   },
