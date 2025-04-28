744415fa9f911421c4e55a751eff98f4621845a0
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-28 14:36:46 +0400

    style: image width and height added

--- pages/MiniGames/GuessColor/GuessColorPage.js
+++ pages/MiniGames/GuessColor/GuessColorPage.js
@@ -1,13 +1,38 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
 const GuessColorPage = () => {
   return (
-    <View>
-      <Text>GUESS</Text>
+    <View style={styles.mainScreen}>
+      <View style={styles.centeredItems}>
+        <View style={styles.centeredItems}>
+          <Image
+            style={styles.image}
+            source={require('../../../assets/images/miniGames/ColorGuess/guessByColor.png')}
+          />
+        </View>
+        <View></View>
+      </View>
     </View>
   );
 };
 
 export default GuessColorPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainScreen: {
+    flex: 1,
+    backgroundColor: '#7985e9',
+  },
+  centeredItems: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {},
+  image: {
+    width: 380,
+    height: 500,
+    objectFit: 'cover',
+  },
+});
