7e259daa1a840a54c58a0a5d625077761790b7ed
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-28 19:33:38 +0400

    chore: DiffucultyOptionButton component

---
+++ components/miniGames/ColorGuess/DifficultyOptionButton.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+
+const DifficultyOptionButton = ({
+  level = 'Easy',
+  seconds = 15,
+  onPress = () => {},
+  backgroundColor = '#f2f2f2',
+  textColor = '#000',
+  secondsColor = '#000', // ✅ yeni eklendi
+  width = 330,
+  height = 60,
+  borderRadius = 16,
+  fontSize = 18,
+  selected = false,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={0.8}
+      style={[
+        styles.button,
+        {
+          backgroundColor: selected ? '#00e6e6' : backgroundColor,
+          width,
+          height,
+          borderRadius,
+        },
+      ]}>
+      <Text style={[styles.levelText, {color: textColor, fontSize}]}>
+        {level}
+      </Text>
+      <Text
+        style={[
+          styles.secondsText,
+          {color: secondsColor, fontSize: fontSize - 2},
+        ]}>
+        {seconds} seconds
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default DifficultyOptionButton;
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginVertical: 8,
+  },
+  levelText: {
+    fontWeight: 'bold',
+  },
+  secondsText: {
+    fontWeight: '600',
+  },
+});
--- pages/MiniGames/GuessColor/DifficultyPage.js
+++ pages/MiniGames/GuessColor/DifficultyPage.js
@@ -1,9 +1,39 @@
 import {StyleSheet, Text, View} from 'react-native';
+import DifficultyOptionButton from '../../../components/miniGames/ColorGuess/DifficultyOptionButton';
+import {setDifficulty} from '../../../src/redux/games/colorGuess/colorGuessSlice';
+import {useDispatch, useSelector} from 'react-redux';
 
 const DifficultyPage = () => {
+  const dispatch = useDispatch();
+  const selectedDifficulty = useSelector(state => state.colorGuess.difficulty);
+
   return (
     <View style={styles.container}>
       <Text>SAAMEYKIM</Text>
+
+      <DifficultyOptionButton
+        level="Easy"
+        seconds={15}
+        selected={selectedDifficulty === 'easy'}
+        secondsColor="green"
+        onPress={() => dispatch(setDifficulty('easy'))}
+      />
+
+      <DifficultyOptionButton
+        level="Medium"
+        seconds={10}
+        selected={selectedDifficulty === 'medium'}
+        secondsColor="orange"
+        onPress={() => dispatch(setDifficulty('medium'))}
+      />
+
+      <DifficultyOptionButton
+        level="Hard"
+        seconds={5}
+        selected={selectedDifficulty === 'hard'}
+        secondsColor="red"
+        onPress={() => dispatch(setDifficulty('hard'))}
+      />
     </View>
   );
 };
