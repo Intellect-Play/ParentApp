45dee89dda4047f44abbd8487ca0654576bf11c9
Mamed766 <mehemmeddemirli57@gmail.com>
2025-05-02 14:18:27 +0400

    refactor: flags updated

--- gameSettings/FlagGuess/flags.js
+++ gameSettings/FlagGuess/flags.js
@@ -123,6 +123,278 @@ const flags = [
     emoji: '🇬🇷',
     answer: 'Greece',
   },
+  {
+    emoji: '🇦🇼',
+    answer: 'Aruba',
+  },
+  {
+    emoji: '🇦🇫',
+    answer: 'Afghanistan',
+  },
+  {
+    emoji: '🇦🇴',
+    answer: 'Angola',
+  },
+  {
+    emoji: '🇦🇮',
+    answer: 'Anguilla',
+  },
+  {
+    emoji: '🇦🇽',
+    answer: 'Åland Islands',
+  },
+  {
+    emoji: '🇦🇱',
+    answer: 'Albania',
+  },
+  {
+    emoji: '🇦🇩',
+    answer: 'Andorra',
+  },
+  {
+    emoji: '🇦🇪',
+    answer: 'United Arab Emirates',
+  },
+  {
+    emoji: '🇦🇲',
+    answer: 'Armenia',
+  },
+  {
+    emoji: '🇦🇸',
+    answer: 'American Samoa',
+  },
+  {
+    emoji: '🇦🇶',
+    answer: 'Antarctica',
+  },
+  {
+    emoji: '🇹🇫',
+    answer: 'French Southern Territories',
+  },
+  {
+    emoji: '🇦🇬',
+    answer: 'Antigua and Barbuda',
+  },
+  {
+    emoji: '🇦🇹',
+    answer: 'Austria',
+  },
+  {
+    emoji: '🇧🇮',
+    answer: 'Burundi',
+  },
+  {
+    emoji: '🇧🇯',
+    answer: 'Benin',
+  },
+  {
+    emoji: '🇧🇶',
+    answer: 'Bonaire, Sint Eustatius and Saba',
+  },
+  {
+    emoji: '🇧🇫',
+    answer: 'Burkina Faso',
+  },
+  {
+    emoji: '🇧🇩',
+    answer: 'Bangladesh',
+  },
+  {
+    emoji: '🇧🇬',
+    answer: 'Bulgaria',
+  },
+  {
+    emoji: '🇧🇭',
+    answer: 'Bahrain',
+  },
+  {
+    emoji: '🇧🇸',
+    answer: 'Bahamas',
+  },
+  {
+    emoji: '🇧🇦',
+    answer: 'Bosnia and Herzegovina',
+  },
+  {
+    emoji: '🇧🇱',
+    answer: 'Saint Barthélemy',
+  },
+  {
+    emoji: '🇧🇾',
+    answer: 'Belarus',
+  },
+  {
+    emoji: '🇧🇿',
+    answer: 'Belize',
+  },
+  {
+    emoji: '🇧🇲',
+    answer: 'Bermuda',
+  },
+  {
+    emoji: '🇧🇴',
+    answer: 'Bolivia, Plurinational State of',
+  },
+  {
+    emoji: '🇧🇧',
+    answer: 'Barbados',
+  },
+  {
+    emoji: '🇧🇳',
+    answer: 'Brunei Darussalam',
+  },
+  {
+    emoji: '🇧🇹',
+    answer: 'Bhutan',
+  },
+  {
+    emoji: '🇧🇻',
+    answer: 'Bouvet Island',
+  },
+  {
+    emoji: '🇧🇼',
+    answer: 'Botswana',
+  },
+  {
+    emoji: '🇨🇫',
+    answer: 'Central African Republic',
+  },
+  {
+    emoji: '🇨🇨',
+    answer: 'Cocos (Keeling) Islands',
+  },
+  {
+    emoji: '🇨🇲',
+    answer: 'Cameroon',
+  },
+  {
+    emoji: '🇨🇩',
+    answer: 'Congo, The Democratic Republic of the',
+  },
+  {
+    emoji: '🇨🇬',
+    answer: 'Congo',
+  },
+  {
+    emoji: '🇨🇰',
+    answer: 'Cook Islands',
+  },
+  {
+    emoji: '🇰🇲',
+    answer: 'Comoros',
+  },
+  {
+    emoji: '🇨🇻',
+    answer: 'Cabo Verde',
+  },
+  {
+    emoji: '🇨🇷',
+    answer: 'Costa Rica',
+  },
+  {
+    emoji: '🇨🇺',
+    answer: 'Cuba',
+  },
+  {
+    emoji: '🇨🇼',
+    answer: 'Curaçao',
+  },
+  {
+    emoji: '🇨🇽',
+    answer: 'Christmas Island',
+  },
+  {
+    emoji: '🇰🇾',
+    answer: 'Cayman Islands',
+  },
+  {
+    emoji: '🇨🇾',
+    answer: 'Cyprus',
+  },
+  {
+    emoji: '🇨🇿',
+    answer: 'Czechia',
+  },
+  {
+    emoji: '🇩🇯',
+    answer: 'Djibouti',
+  },
+  {
+    emoji: '🇩🇲',
+    answer: 'Dominica',
+  },
+  {
+    emoji: '🇩🇰',
+    answer: 'Denmark',
+  },
+  {
+    emoji: '🇩🇴',
+    answer: 'Dominican Republic',
+  },
+  {
+    emoji: '🇩🇿',
+    answer: 'Algeria',
+  },
+  {
+    emoji: '🇪🇨',
+    answer: 'Ecuador',
+  },
+  {
+    emoji: '🇪🇬',
+    answer: 'Egypt',
+  },
+  {
+    emoji: '🇪🇷',
+    answer: 'Eritrea',
+  },
+  {
+    emoji: '🇪🇭',
+    answer: 'Western Sahara',
+  },
+  {
+    emoji: '🇪🇪',
+    answer: 'Estonia',
+  },
+  {
+    emoji: '🇪🇹',
+    answer: 'Ethiopia',
+  },
+  {
+    emoji: '🇫🇯',
+    answer: 'Fiji',
+  },
+  {
+    emoji: '🇫🇰',
+    answer: 'Falkland Islands (Malvinas)',
+  },
+  {
+    emoji: '🇫🇴',
+    answer: 'Faroe Islands',
+  },
+  {
+    emoji: '🇫🇲',
+    answer: 'Micronesia, Federated States of',
+  },
+  {
+    emoji: '🇬🇦',
+    answer: 'Gabon',
+  },
+  {
+    emoji: '🇬🇪',
+    answer: 'Georgia',
+  },
+  {
+    emoji: '🇬🇬',
+    answer: 'Guernsey',
+  },
+  {
+    emoji: '🇬🇭',
+    answer: 'Ghana',
+  },
+  {
+    emoji: '🇬🇮',
+    answer: 'Gibraltar',
+  },
 ];
 
 export default flags;
